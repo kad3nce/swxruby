a8798ed02f98c2e169cd0f81aa1c75b0d326c04c
Jed Hurt <jedhurt@cableone.net>
2008-03-11 22:52:19 -0600

    Wrote RDOC formatted comments for merb standalone server

--- examples/standalone/standalone.rb
+++ examples/standalone/standalone.rb
@@ -1,4 +1,19 @@
-$:.unshift '/Users/Jed/Development/Libraries/swxruby/lib'
+# = Merb Standalone SWX Ruby Server
+# 
+# An example of how simple it is to build a standalone SWX Ruby server using Merb.
+# 
+# == Prerequisites
+# 
+# * SWX Ruby Gem (gem install swxruby)
+# * merb-core 0.9.1 or later (gem install merb-core)
+# 
+# == Take it for a spin
+# 
+# * Open a terminal in the examples/standalone directory and execute 'merb -I standalone.rb' to fire up the Merb server.
+# * Open standalone.fla in Flash 8 or later.
+# * Publish Preview (ctrl+enter on Windows; cmd+enter on OS X).
+# * Watch as the output panel begins tracing "Hello from Merb!".
+require 'rubygems'
 require 'swxruby'
 
 # =================
