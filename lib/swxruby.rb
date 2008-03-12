6b01efe6c0b8c2d54d0485a4386bc25e36eae217
Jed Hurt <jedhurt@cableone.net>
2008-03-11 19:58:18 -0600

    Removed "Loading SWX Ruby" banner

--- lib/swxruby.rb
+++ lib/swxruby.rb
@@ -1,8 +1,4 @@
 $:.unshift File.dirname(__FILE__)
 $:.unshift File.join(File.dirname(__FILE__), 'swxruby')
 
-puts "
-# ====================
-# = Loading Swx Ruby =
-# ===================="
 require 'swx_gateway'
